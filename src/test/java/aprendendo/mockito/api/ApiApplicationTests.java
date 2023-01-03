6c85a09cba396ed52ed6e016bf4c4949e6e2230e
Fernando Guedes <nando3ryu@gmail.com>
2023-01-03 15:51:53 -0300

    Curso Mockito 1/ commit

---
+++ src/test/java/aprendendo/mockito/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aprendendo.mockito.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
