d8c19e6777f0bd254b1532494423837a8920d564
Fernando Guedes <nando3ryu@gmail.com>
2023-01-18 10:37:28 -0300

    Exceção para e-mail já cadastrado e excluindo Model User.

---
+++ src/main/java/aprendendo/mockito/api/services/exceptions/DataIntegratvViolationException.java
@@ -0,0 +1,8 @@
+package aprendendo.mockito.api.services.exceptions;
+
+public class DataIntegratvViolationException extends RuntimeException {
+
+    public DataIntegratvViolationException(String message) {
+        super(message);
+    }
+}
